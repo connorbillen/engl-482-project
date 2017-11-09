ce93cd77c4d76873fb9464cff49bc7041733ee34
Michael Incardona <mji8299@rit.edu>
2017-11-09 12:29:45 -0500

    Create dependency script

---
+++ pip3-depends.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "Warning: not root"
+fi
+
+USE_APT=1
+command -v apt-get >/dev/null 2>&1 || USE_APT=0
+
+if [ "$USE_APT" -ne "0" ]; then
+    echo "Found apt command. Installing apt packages..."
+    apt-get install python3
+    apt-get install pip3
+else
+    echo "Did not find apt command. Make sure you install the necessary packages."
+fi
+
+echo "Installing pip3 packages..."
+pip3 install bs4
