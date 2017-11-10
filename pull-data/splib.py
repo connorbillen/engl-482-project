b8e05e4cb30408b4a15a7c392f1d5bf3b942f1f0
Michael Incardona <mji8299@rit.edu>
2017-11-10 15:12:53 -0500

    Script now pulls mp3, gif, and text speech

--- pull-data/splib.py
+++ pull-data/splib.py
@@ -3,29 +3,58 @@
 import string
 import urllib.request
 from bs4 import BeautifulSoup
+import os
 
 def clean_string(s):
     # replaces non-breaking spaces with spaces and collapses all whitespace runs
     return " ".join(str(s).replace("\u00A0", " ").split())
 
-# for right now, just scrapes the given speaker_id page for the
-# urls and english text and prints them
-def fetch_accent_archive(speaker_id, target_directory):
-    page_url = "http://accent.gmu.edu/browse_language.php?function=detail&speakerid={0}".format(speaker_id)
+# gets a urllib(2) file handle to a remote file
+def get_net_file_handle(fileurl):
     try:
-        resp_webpage = urllib.request.urlopen(page_url)
+        webfile = urllib.request.urlopen(fileurl)
     except URLError as e:
         raise e
 
-    # http response code
-    req_code = resp_webpage.getcode()
-    # if the code isn't some "OK" variant
+    req_code = webfile.getcode()
     if req_code < 200 or req_code >= 300:
-        raise Exception("Unable to get accent archive page: HTTP code {0}".format(req_code))
+        raise IOError("HTTP error {0} connecting to {1}".format(req_code, fileurl))
+
+    return webfile
+
+# reads a remote file and returns the contents
+def get_net_file_contents(fileurl):
+    with get_net_file_handle(fileurl) as net_file_handle:
+        content = net_file_handle.read()
+    return content
+
+# basically wget: fetch data from fileurl over http and dump it
+# in a file named localpath
+def save_net_file(fileurl, localpath):
+    with get_net_file_handle(fileurl) as net_file_handle:
+        with open(localpath, "wb") as target_file:
+            # bytes is a "bytes object"
+            byte = net_file_handle.read(1)
+            while byte != b"":
+                target_file.write(byte)
+                byte = net_file_handle.read(1)
+
+def overwrite_text(text, localpath):
+    with open(localpath, "w") as target_file:
+        target_file.write(text)
+
+def extract_extension(fname):
+    return ""
+
+# for right now, just scrapes the given speaker_id page for the
+# urls and english text and prints them
+def fetch_accent_archive(speaker_id, target_directory):
+    target_directory += "/"
+    os.makedirs(target_directory, exist_ok=True)
+    page_url = "http://accent.gmu.edu/browse_language.php?function=detail&speakerid={0}".format(speaker_id)
 
     # get the html
-    html_content = resp_webpage.read()
-    resp_webpage.close()
+    html_content = get_net_file_contents(page_url)
 
     # build a document tree
     soup = BeautifulSoup(html_content, "html.parser")
@@ -40,7 +69,16 @@ def fetch_accent_archive(speaker_id, target_directory):
     print("Image: ", image_url)
     print("Speech text: ", speech_text)
 
-    return
+    html_content = None
+    overwrite_text(speech_text, target_directory + "english.txt")
+    speech_text = None
+    save_net_file(audio_url, target_directory + "audio.mp3")
+    save_net_file(image_url, target_directory + "ipa.gif")
 
 if __name__ == "__main__":
-    fetch_accent_archive(86, "data")
+    archive_id = 86
+    try:
+        fetch_accent_archive(archive_id, "test-data/{0}".format(archive_id))
+    except Exception as e:
+        print("Error fetching data for subject id {0}: {1}".format(86, e))
+
