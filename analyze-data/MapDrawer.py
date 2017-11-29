6f22b0f5f0bb151f9f6bf8675d61f615d9ef461b
Connor Billen <me@connorbillen.com>
2017-11-28 22:47:06 -0500

    Made title actually matter

--- analyze-data/MapDrawer.py
+++ analyze-data/MapDrawer.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap as Basemap
 
 class MapDrawer:
-    def __init__(self, title):
+    def __init__(self, title=''):
         self.m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
                     projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
         self.shp_info = self.m.readshapefile('st99_d00','states',drawbounds=True)
         self.colors = ['r', 'b', 'g', 'c', 'k', 'm', 'y']
         self.color_i = 0
-        plt.title('Science & Analytics of Speech')
+        plt.title(title)
        
     def add_points(self, lats, lons):
         lons, lats = self.m(lons, lats)
