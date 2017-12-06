3f985e73b4c66bfaa188dfc32a7c3c4f1a9ee1b3
Connor Billen <me@connorbillen.com>
2017-12-06 16:40:16 -0500

    Template file for reading in all valid speaker data

--- analyze-data/template_analysis.py
+++ analyze-data/template_analysis.py
@@ -6,17 +6,23 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 class Speaker:
-    def __init__(self, lat, lon, words, male, age):
+    def __init__(self, lat, lon, words, male=True, age=63):
         self.lat = lat
         self.lon = lon
         self.words = words
         self.male = male
         self.age = age
 
-for speaker in os.lisdir('../test-data'):
+speakers = []
+
+for speaker in os.listdir('../test-data'):
     try:
         transcription = open('../test-data/' + speaker + '/audio_arpabet.txt').read()
         latitude, longitude = open('../test-data/' + speaker + '/location.txt').read().split('\n')[1].split(',')
+        age, male = open('../test-data/' + speaker + '/socio.txt').read().split('\n') 
         speakers.append(Speaker(float(latitude), float(longitude), transcription))
     except:
         print('Unable to process info for Speaker', speaker)
+
+for speaker in speakers:
+    # logic
